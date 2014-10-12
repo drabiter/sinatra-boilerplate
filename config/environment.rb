74fe51442464f8bb1ad26372f67395b1191b3fba
Hendra G <drabiter@gmail.com>
2014-10-12 15:39:25 +0700

    Add database env

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require 'yaml'
 ENV['RACK_ENV'] ||= :development.to_s
 
 db_options = YAML.load File.read('./config/database.yml')
-ActiveRecord::Base.establish_connection db_options
+ActiveRecord::Base.establish_connection db_options[ENV['RACK_ENV']]
